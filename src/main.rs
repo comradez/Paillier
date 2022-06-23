142c0788407cbf9b91e87f96cefaea1bfb3bfe86
ChrisZhang <zcyjim@outlook.com>
2022-06-23 16:01:06 +0800

    Finish a hot-seat Paillier algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 mod keygen;
 mod process;
+
+use std::num::ParseIntError;
 use num_primes::BigUint;
 use process::decrypt;
 use process::encrypt;
+
+fn read_u32() -> Result<u32, ParseIntError> {
+    let mut n = String::new();
+    std::io::stdin().read_line(&mut n).unwrap();
+    n.trim().parse()
+}
+
 fn main() {
-    let (public_key, private_key) = keygen::gen_key(512_usize);
-    // println!("public key: ({}, {})", public_key.0, public_key.1);
-    // println!("private key: ({}, {})", private_key.0, private_key.1);
-    let message = BigUint::from(3_usize);
-    let secret  = encrypt(&message, &public_key, None);
-    println!("message: {}", message);
-    println!("secret: {}", &secret);
-    println!("restored: {}", decrypt(&secret, &public_key, &private_key))    
-    // println!("private key: {}", private_key);
+    let (public_key, private_key) = keygen::gen_key(64_usize);
+    println!("Pleas input user number:");
+    let n = read_u32().unwrap();
+    let mut vote_sum = 0u32;
+    let mut encrypted_vote_prod: BigUint = 1u32.into();
+
+    for i in 0 .. n {
+        println!("User {}, please vote:", i);
+        let vote = read_u32().unwrap();
+        vote_sum += vote;
+        let encrypted_vote = encrypt(&vote.into(), &public_key, None);
+        encrypted_vote_prod *= &encrypted_vote;
+        assert_eq!(decrypt(&encrypted_vote, &public_key, &private_key), vote.into());
+    }
+
+    let decrypted_vote_sum = decrypt(&encrypted_vote_prod, &public_key, &private_key);
+    assert_eq!(decrypted_vote_sum, vote_sum.into());
+    println!("Decrypted vote sum is {}, result validated.", decrypted_vote_sum);
 }
--- src/process.rs
+++ src/process.rs
@@ -1,20 +1,19 @@
 use num_primes::{RandBigInt, BigUint};
 
-use crate::keygen::{l, quick_pow};
+use crate::keygen::l;
 
 pub fn encrypt(message: &BigUint, key: &(BigUint, BigUint), r: Option<BigUint>) -> BigUint {
     let (n, g) = key;
     let r = r.map_or_else(|| {
         rand::thread_rng().gen_biguint_below(n)
     }, |r| r % n);
-    println!("r is {}", &r);
     let n_sq = n * n;
-    quick_pow(&g, &message, &n_sq) * quick_pow(&r, &n, &n_sq) % n_sq
+    g.modpow(message, &n_sq) * r.modpow(n, &n_sq) % n_sq
 }
 
 pub fn decrypt(secret: &BigUint, public_key: &(BigUint, BigUint), private_key: &(BigUint, BigUint)) -> BigUint {
     let (n, _) = public_key;
     let (lambda, mu) = private_key;
     let n_sq = n * n;
-    l(&quick_pow(secret, lambda, &n_sq), n) * mu % n
+    l(&secret.modpow(lambda, &n_sq), n) * mu % n
 }
